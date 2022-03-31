4cd405367c64f6a4d38b247d05034e42529879c4
DoktorIgnoranz <hendrik-behle@outlook.com>
2022-03-31 10:25:37 +0200

    Create DataTypes.java

---
+++ DataTypes.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		int Eins = 1;
+		short Zwei = 2;
+		byte Drei = 3;
+		float Vier = 4;
+		double Fuenf = 5.5;
+		System.out.print("Ergebnis der rechnung: ");
+		System.out.println(Eins + Zwei);
+	}
+
+}
